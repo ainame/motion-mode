a7cf7f88a4ec2a402f9e277b42536f01e7f79532
ainame <ainame954@facebook.com>
2013-03-21 20:45:48 +0900

    update sample setting

--- motion-mode-conf-sample.el
+++ motion-mode-conf-sample.el
@@ -1,6 +1,8 @@
 (require 'motion-mode)
 (add-to-list 'ac-modes 'motion-mode)
+(add-to-list 'ac-sources 'ac-source-dictionary)
 (add-hook 'ruby-mode-hook 'motion-upgrade-major-mode-if-motion-project)
+(define-key motion-mode-map (kbd "C-c C-c") 'motion-execute-rake)
 
 
 
