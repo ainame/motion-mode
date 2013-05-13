c58afa409151601cf7cfd1008c419303abbb394d
Eiji Iwazawa <iwazawa@gmail.com>
2013-05-13 15:06:13 +0900

    fix 'invalid byte sequence in US-ASCII'
    変換しようとしたObjective-CのコードにUS-ASCII以外の文字列が含まれると
    `gsub!': invalid byte sequence in US-ASCII
    というエラーで落ちるのをUTF-8で書かれていると決め打ちして修正しました。

--- bin/code_converter.rb
+++ bin/code_converter.rb
@@ -2,6 +2,6 @@
 $:.unshift File.expand_path(File.dirname(__FILE__) + '/../lib')
 require 'motion/code_converter'
 
-code = $stdin.read
+code = $stdin.set_encoding("UTF-8").read
 cnv  = Motion::CodeConverter.new(code)
 puts cnv.result
